2686ceb471c6afc6fd8fbb6f6b8f6b784a5e6bf3
ADEEL-AHMAD-123 <132742975+ADEEL-AHMAD-123@users.noreply.github.com>
2025-08-08 21:26:10 +0500

    fixed use of middleware for Acoount function

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,14 +2,14 @@
 const express = require('express');
 const router = express.Router();
 const { register, login,logout,getLoggedInUser } = require('../controllers/authController');
-
+const { protect, adminOnly } = require('../middlewares/authMiddleware');
 // @route   POST /api/auth/register
 router.post('/register', register);
 
 // @route   POST /api/auth/login
 router.post('/login', login);
 
-router.get('/me', getLoggedInUser);
+router.get('/me', protect,getLoggedInUser);
 
 router.post('/logout', logout);
 
