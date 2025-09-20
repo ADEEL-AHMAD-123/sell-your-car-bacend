e6c08c7ca90031025fdf90c3f14835a49e2516da
ADEEL-AHMAD-123 <adeeel5598@gmail.com>
2025-09-20 22:47:21 +0500

    improved quote deletion logic

--- utils/promoEmailService.js
+++ utils/promoEmailService.js
@@ -1,36 +1,33 @@
-// utils/promoEmailService.js
 const nodemailer = require("nodemailer");
 const path = require("path");
 const ejs = require("ejs");
-const dotenv = require('dotenv');
-dotenv.config();
 
 // transporter for promotional emails via Mailjet
 const promoTransporter = nodemailer.createTransport({
-  host: process.env.MAILJET_HOST,
-  port: process.env.MAILJET_PORT,
-  secure: false, // Use 'true' if port is 465
-  auth: {
-    user: process.env.MAILJET_USER, 
-    pass: process.env.MAILJET_PASS,
-  },
+  host: "in-v3.mailjet.com",
+  port: 587,
+  secure: false, 
+  auth: {
+    user: "475bd7af2c9c667b5d18a8dbf567c951", 
+    pass: "a21004a9cacbc75722bbf6c9e496df31",
+  },
 });
 
 const sendPromoEmail = async ({ to, subject, templateName, templateData }) => {
-  try {
-    const templatePath = path.join(__dirname, "..", "emails", `${templateName}.ejs`);
-    const html = await ejs.renderFile(templatePath, templateData);
+  try {
+    const templatePath = path.join(__dirname, "..", "emails", `${templateName}.ejs`);
+    const html = await ejs.renderFile(templatePath, templateData);
 
-    await promoTransporter.sendMail({
-      from: `SellYourCar <promo@sellyourcar.info>`, 
-      to,
-      subject,
-      html,
-    });
-  } catch (err) {
-    console.error("Promotional email failed:", err.message);
-    throw new Error("Failed to send promotional email.");
-  }
+    await promoTransporter.sendMail({
+      from: `SellYourCar <promo@sellyourcar.info>`, 
+      to,
+      subject,
+      html,
+    });
+  } catch (err) {
+    console.error("Promotional email failed:", err.message);
+    throw new Error("Failed to send promotional email.");
+  }
 };
 
 module.exports = sendPromoEmail;
